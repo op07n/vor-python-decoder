33e2196ace2e7e4a21072d1d3354f833b23ac818
op07n <39182105+op07n@users.noreply.github.com>
2019-12-06 13:27:32 +0100

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,15 @@
+mkdir -p ~/.streamlit/
+
+echo "\
+    [general]\n\
+    email = \"your-email@domain.com\"\n\
+    " > ~/.streamlit/credentials.toml
+
+echo "\
+    [server]\n\
+    headless = true\n\
+    enableCORS = false
+    port = $PORT\n\
+    [browser]\n\
+    gatherUsageStats = false\n\
+    " > ~/.streamlit/config.toml
